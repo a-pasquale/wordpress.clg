55563002e7047af4eff7453ecb65db92733f07b1
Andrew Pasquale <andrew@elytra.net>
2013-02-01 09:38:56 -0500

    Add row.

--- wp-content/themes/ari/index.php
+++ wp-content/themes/ari/index.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 
 <div id="main" class="span8">
-
+  <div class="row">
 	<div id="content">
 		<?php get_template_part( 'loop', 'index' );	?>
 	</div>
 	<!--end Content-->
-
+  </div> <!-- end of row -->
 <!--end Main-->
 
 <?php get_footer(); ?>
